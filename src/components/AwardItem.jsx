ab2297352303851504174be422dcd9f3fac70cdc
ryan-lam <ryan-lam@users.noreply.github.com>
2022-01-15 00:27:20 -0500

    done page but need to fix auto screen size so content wraps around

--- src/components/AwardItem.jsx
+++ src/components/AwardItem.jsx
@@ -1,21 +1,37 @@
 import React from 'react';
-
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../index.css';
 import styles from './AwardItem.module.css';
 
+// Component to add title and horizontal line
+// Year: Number of year (Int)
+const AwardYear = ({ Year }) => {
+  return (
+    <div>
+      <h3 className={`${styles.h3__year}`}>{Year}</h3>
+      <hr width="95%" />
+    </div>
+  );
+};
+AwardYear.defaultProps = {
+  Year: 9999,
+};
+
+// Component to add a singular award
+// RocketName: Name of Rocket (Str)
+// AwardName: Name of Award (Str)
+// AwardDescription: Description of Award (Str)
+// ImageAddress: Address of Image relative to Award.jsx file (Image)
+// ImageName: Name of Image for alt tag (Str)
 const AwardItem = ({
   RocketName, AwardName, AwardDescription, ImageAddress, ImageName,
 }) => {
   return (
-
     <div className={`${styles.award__wrapper}`}>
-
-      <div className={`col-md-1 ${styles.content_rocket_tag}`}>
+      <div className={`${styles.content_rocket_tag}`}>
         <h1>{RocketName}</h1>
       </div>
-
-      <div className={`col-sm-3 ${styles.award_body}`}>
+      <div className={`${styles.award_body}`}>
         <h2>{AwardName}</h2>
         <div className={`${styles.line}`} />
         <div className={`${styles.content_text}`}>
@@ -24,15 +40,12 @@ const AwardItem = ({
           </p>
         </div>
       </div>
-
-      <div className="col-sm-2">
-        <img className={`${styles.poster}`} src="https://www.waterloorocketry.com/images/awards/30k_SRAD19.png" alt="Test" />
+      <div>
+        <img className={`${styles.poster}`} src={ImageAddress} alt={ImageName} />
       </div>
-
     </div>
   );
 };
-
 AwardItem.defaultProps = {
   RocketName: 'RocketName',
   AwardName: 'AwardName',
@@ -41,4 +54,4 @@ AwardItem.defaultProps = {
   ImageName: 'AwardDescription',
 };
 
-export default AwardItem;
+export { AwardItem, AwardYear };
--- src/components/routes/Awards.jsx
+++ src/components/routes/Awards.jsx
@@ -1,10 +1,12 @@
 import { React } from 'react';
-import { Col, Container, Row } from 'react-bootstrap';
-
+import { Container } from 'react-bootstrap';
 import styles from '../AwardItem.module.css';
-import AwardItem from '../AwardItem';
+import { AwardItem, AwardYear } from '../AwardItem';
 
-// How to add awards:
+import SRAD19Image from '../../img/awards/30k_SRAD19.png';
+import SRAD18Image from '../../img/awards/10k_SRAD18.png';
+import SRAD17Image from '../../img/awards/10k_SRAD17.png';
+import TechincalExcellenceImage from '../../img/awards/technical_excellence.png';
 
 const Awards = () => {
   return (
@@ -12,37 +14,47 @@ const Awards = () => {
       <div id={`${styles.awards_cover}`} />
       <Container fluid>
 
-        {/* 2019 */}
-        <div className={`${styles.year}`}>
-          <h3 className={`${styles.h3__year}`}>2019</h3>
-          <hr width="95%" />
+        <div>
+          <AwardYear Year={2019} />
           <AwardItem
             RocketName="SotS"
             AwardName="SECOND PLACE - 30K LIQUID/HYBRID SRAD"
             AwardDescription="The Category Second Place Awards recognize the teams with the second highest overall scores in each of the six competition categories, with this category representing Student Researched and Developed (SRAD) Hybrid/Liquid engines targeting an apogee of 30000 feet, moving us a division up from last year. Teams are scored on the project technical report, competency of design, quality of construction, degree of student development, flight performance, and operating procedures. Our team received this award for finishing second in our category at the 2019 IREC, with the debut flight of SOTS."
+            ImageAddress={SRAD19Image}
+            ImageName="Second Place - 30k Liquid/Hybrid Srad"
           />
         </div>
 
-        {/* 2018 */}
-        <div className={`${styles.year}`}>
-          <h3>2018</h3>
-          <hr width="95%" />
+        <div>
+          <AwardYear Year={2018} />
           <AwardItem
             RocketName="UXO"
             AwardName="FIRST PLACE - 10K LIQUID/HYBRID SRAD"
-            AwardDescription="The Category First Place Awards recognize the teams with the highest overall scores in each of the six competition categories, with this category representing Student Researched and Developed (SRAD) Hybrid/Liquid engines targeting an apogee of 10000 feet. Teams are scored on the project technical report, competency of design, quality of construction, degree of student development, flight performance, and operating procedures. Our team received this award for finishing first in our category at the 2018 IREC, with the debut flight of UXO."
+            AwardDescription="The Category First Place Awards recognize the teams with the highest overall scores in each of the six competition categories, with this category representing Student Researched and Developed (SRAD) Hybrid/Liquid engines targeting an apogee of 10,000 feet. Teams are scored on the project technical report, competency of design, quality of construction, degree of student development, flight performance, and operating procedures. Our team received this award for finishing first in our category at the 2018 IREC, with the debut flight of UXO."
+            ImageAddress={SRAD18Image}
+            ImageName="First Place - 10k Liquid/Hybrid Srad"
           />
         </div>
 
-        {/* 2017 */}
-        <div className={`${styles.year}`}>
-          <h3>2017</h3>
-          <hr width="95%" />
-          <AwardItem RocketName="VIDAR III" />
-          <AwardItem RocketName="VIDAR III" />
+        <div>
+          <AwardYear Year={2017} />
+          <AwardItem
+            RocketName="VIDAR III"
+            AwardName="JIM FURFARO AWARD FOR TECHNICAL EXCELLENCE"
+            AwardDescription="The Jim Furfaro Award for Technical Excellence recognizes a team which demonstrates exceptional engineering discipline and technical skill through their analyses and conclusions, project or program planning and execution, operational procedure, manufacturing processes, iterative improvement, systems engineering methodology, and robust design. Our team was presented with this award after our successful launch and recovery of Vidar III at the 2017 IREC, based on our iterative design on the Vidar system over the previous four years."
+            ImageAddress={TechincalExcellenceImage}
+            ImageName="Jim Furfaro Award for Technical Excellence"
+          />
+          <AwardItem
+            RocketName="VIDAR III"
+            AwardName="FIRST PLACE - 10K LIQUID/HYBRID SRAD"
+            AwardDescription="The Category First Place Awards recognize the teams with the highest overall scores in each of the six competition categories, with this category representing Student Researched and Developed (SRAD) Hybrid/Liquid engines targeting an apogee of 10,000 feet. Teams are scored on the project technical report, competency of design, quality of construction, degree of student development, flight performance, and operating procedures. Our team received this award for finishing first in our category at the 2017 IREC."
+            ImageAddress={SRAD17Image}
+            ImageName="First Place - 10k Liquid/Hybrid Srad"
+          />
         </div>
-      </Container>
 
+      </Container>
     </>
   );
 };
