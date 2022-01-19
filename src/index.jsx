2c187d32f61c8f1fbd68a8d7e8bb2bd42a4c0d49
NitpreetB <65519637+NitpreetB@users.noreply.github.com>
2022-01-19 11:21:28 -0500

    fixed merging issue on idex.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,33 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {
-  Route, BrowserRouter, Routes as Switch,
+  Route, BrowserRouter, Routes,
 } from 'react-router-dom';
 
 import Navigation from './components/Navigation';
 import Page from './components/Page';
 import Home from './components/routes/Home';
-import Competition from './components/routes/competition';
 import Footer from './components/Footer';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 
 ReactDOM.render(
-  <BrowserRouter>
-    <Navigation />
-    <Switch>
-      <Route
-        path="/"
-        element={<Page title="Home"><Home /></Page>}
-      />
-      <Route
-        path="/competition"
-        element={<Page title="Competition"><Competition /></Page>}
-      />
-    </Switch>
-    <Footer />
-  </BrowserRouter>,
+  <React.StrictMode>
+    <BrowserRouter basename="/website-react">
+      <Navigation />
+      <Routes>
+        <Route
+          exact
+          path="/"
+          element={<Page title="Home"><Home /></Page>}
+        />
+        <Route
+          path="/competition"
+          element={<Page title="Competition"><Home /></Page>}
+        />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  </React.StrictMode>,
 
   document.getElementById('root'),
 );
