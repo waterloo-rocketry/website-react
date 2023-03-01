f375357150446837c9b3eebea703b1cc32cdb144
lucasreljic <lucas.reljic@gmail.com>
2023-03-01 16:26:44 -0500

    fixed header spacing

--- src/components/TwoColumns/TwoColumns.jsx
+++ src/components/TwoColumns/TwoColumns.jsx
@@ -6,7 +6,7 @@ import styles from './TwoColumns.module.css';
 const TwoColumns = ({ title, children }) => {
   return (
     <>
-      <h2>{title}</h2>
+      <h2 className={styles.title}>{title}</h2>
       <Row className={styles.container}>
         <Col className={styles.leftColumn}>{children[0]}</Col>
         <Col className={styles.rightColumn}>{children[1]}</Col>
