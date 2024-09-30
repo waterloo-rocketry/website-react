f48a355574869462e2778704c64f00111e06caf4
Manav <manavhockey13@gmail.com>
2024-09-30 16:26:04 -0400

    added working links to wrkrprts

--- src/routes/Documentation.jsx
+++ src/routes/Documentation.jsx
@@ -114,108 +114,108 @@ const Documentation = () => {
             <ul className="competition-reports-link-list">
               <li>
                 Increasing Rocket Apogee by 23% Through Iterative Design - Joel Godard (Jan 2024) |
-                <a href="https://www.waterloorocketry.com/pdfs/JoelGodardJan2024.pdf" target="_blank" rel="noreferrer">View</a>
+                <a href="https://drive.google.com/file/d/1WCfqrq5eObcD8L7Ti5hjObDkq3xk2J32/preview" target="_blank" rel="noreferrer">View</a>
                 {' '}
                 |
-                <a href="https://www.waterloorocketry.com/pdfs/JoelGodardJan2024.pdf" download>Download</a>
+                <a href="https://drive.google.com/uc?export=download&id=1WCfqrq5eObcD8L7Ti5hjObDkq3xk2J32" download>Download</a>
               </li>
               <li>
                 The Design of the Recovery Electronics Module for the 2023 IREC Competition - Stefan Arroyo-Cottier (Jan 2024) |
-                <a href="https://www.waterloorocketry.com/pdfs/StefanArroyo-CottierJan2024.pdf" target="_blank" rel="noreferrer">View</a>
+                <a href="https://drive.google.com/file/d/1GSmT4Rn4Gx0krKcMmN34iBY3zURa2_Bi/preview" target="_blank" rel="noreferrer">View</a>
                 {' '}
                 |
-                <a href="https://www.waterloorocketry.com/pdfs/StefanArroyo-CottierJan2024.pdf" download>Download</a>
+                <a href="https://drive.google.com/uc?export=download&id=1GSmT4Rn4Gx0krKcMmN34iBY3zURa2_Bi" download>Download</a>
               </li>
               <li>
                 The Design of a Testable Hybrid Rocket Engine Using Software Analysis - Artem Sotnikov (Jan 2023) |
-                <a href="https://www.waterloorocketry.com/pdfs/ArtemSotnikovJan2023.pdf" target="_blank" rel="noreferrer">View</a>
+                <a href="https://drive.google.com/file/d/1S1HPG4jcGP1DqZBhM8gB1YVeVin-YZPW/preview" target="_blank" rel="noreferrer">View</a>
                 {' '}
                 |
-                <a href="https://www.waterloorocketry.com/pdfs/ArtemSotnikovJan2023.pdf" download>Download</a>
+                <a href="https://drive.google.com/uc?export=download&id=1S1HPG4jcGP1DqZBhM8gB1YVeVin-YZPW" download>Download</a>
               </li>
               <li>
                 A Practical Method For Reducing Skin Friction Drag in Sounding Rockets - Aaron Leszkowiat (Sep 2023) |
-                <a href="https://www.waterloorocketry.com/pdfs/AaronLeszkowiatSep2023.pdf" target="_blank" rel="noreferrer">View</a>
+                <a href="https://drive.google.com/file/d/13vonNjU3d0A9C-SAtyrdvax5SVbD-dHY/preview" target="_blank" rel="noreferrer">View</a>
                 {' '}
                 |
-                <a href="https://www.waterloorocketry.com/pdfs/AaronLeszkowiatSep2023.pdf" download>Download</a>
+                <a href="https://drive.google.com/uc?export=download&id=13vonNjU3d0A9C-SAtyrdvax5SVbD-dHY" download>Download</a>
               </li>
               <li>
                 Design and Testing of a Coaxial Propellant Valve - Michael Zhou (Sep 2023) |
-                <a href="https://www.waterloorocketry.com/pdfs/MichaelZhouSep2023.pdf" target="_blank" rel="noreferrer">View</a>
+                <a href="https://drive.google.com/file/d/1WzVx9eEEpdomRLxhcIoJ7r-5EQDsG41A/preview" target="_blank" rel="noreferrer">View</a>
                 {' '}
                 |
-                <a href="https://www.waterloorocketry.com/pdfs/MichaelZhouSep2023.pdf" download>Download</a>
+                <a href="https://drive.google.com/uc?export=download&id=1WzVx9eEEpdomRLxhcIoJ7r-5EQDsG41A" download>Download</a>
               </li>
               <li>
                 Design of a Weatherproof Laptop for Rocketry Operations - Zachariah Mears (Sep 2022) |
-                <a href="https://www.waterloorocketry.com/pdfs/ZachariahMearsSep2022.pdf" target="_blank" rel="noreferrer">View</a>
+                <a href="https://drive.google.com/file/d/1cQ5RsS397zvtpukP9yabZGdcUj8TN_J7/preview" target="_blank" rel="noreferrer">View</a>
                 {' '}
                 |
-                <a href="https://www.waterloorocketry.com/pdfs/ZachariahMearsSep2022.pdf" download>Download</a>
+                <a href="https://drive.google.com/uc?export=download&id=1cQ5RsS397zvtpukP9yabZGdcUj8TN_J7" download>Download</a>
               </li>
               <li>
                 Flightworthiness Verification of a 3U CubeSat for Sounding Rocket Payloads with Ansys FEA - Zhen Bo Bian (Jan 2021)
-                <a href="https://www.waterloorocketry.com/pdfs/ZhenBoBianJan2021.pdf" target="_blank" rel="noreferrer">View</a>
+                <a href="https://drive.google.com/file/d/1svfrbJoNVIn8QLqsAxZuygPx-Sminuqw/preview" target="_blank" rel="noreferrer">View</a>
                 {' '}
                 |
-                <a href="https://www.waterloorocketry.com/pdfs/ZhenBoBianJan2021.pdf" download>Download</a>
+                <a href="https://drive.google.com/uc?export=download&id=1svfrbJoNVIn8QLqsAxZuygPx-Sminuqw" download>Download</a>
               </li>
               <li>
                 Design of a Remote Arming System for Sounding Rocket Recovery Systems - Zachariah Mears (May 2021) |
-                <a href="https://www.waterloorocketry.com/pdfs/ZachariahMearsMay2021.pdf" target="_blank" rel="noreferrer">View</a>
+                <a href="https://drive.google.com/file/d/1svfrbJoNVIn8QLqsAxZuygPx-Sminuqw/preview" target="_blank" rel="noreferrer">View</a>
                 {' '}
                 |
-                <a href="https://www.waterloorocketry.com/pdfs/ZachariahMearsMay2021.pdf" download>Download</a>
+                <a href="https://drive.google.com/uc?export=download&id=1svfrbJoNVIn8QLqsAxZuygPx-Sminuqw" download>Download</a>
               </li>
               <li>
                 The Design of a Reefing Parachute Recovery System for a Student Competition Sounding Rocket - Artem Sotnikov (Sep 2021) |
-                <a href="https://www.waterloorocketry.com/pdfs/ArtemSotnikovSep2021.pdf" target="_blank" rel="noreferrer">View</a>
+                <a href="https://drive.google.com/file/d/1vLr4iOF98OzdV2tspgNxmfHTgOtGnNoY/preview" target="_blank" rel="noreferrer">View</a>
                 {' '}
                 |
-                <a href="https://www.waterloorocketry.com/pdfs/ArtemSotnikovSep2021.pdf" download>Download</a>
+                <a href="https://drive.google.com/uc?export=download&id=1vLr4iOF98OzdV2tspgNxmfHTgOtGnNoY" download>Download</a>
               </li>
               <li>
                 Design and Analysis of a Thrust Structure for a Hybrid Sounding Rocket - Shirley Kong (Sep 2021) |
-                <a href="https://www.waterloorocketry.com/pdfs/ShirleyKongSep2021.pdf" target="_blank" rel="noreferrer">View</a>
+                <a href="https://drive.google.com/file/d/1vLr4iOF98OzdV2tspgNxmfHTgOtGnNoY/preview" target="_blank" rel="noreferrer">View</a>
                 {' '}
                 |
-                <a href="https://www.waterloorocketry.com/pdfs/ShirleyKongSep2021.pdf" download>Download</a>
+                <a href="https://drive.google.com/uc?export=download&id=1vLr4iOF98OzdV2tspgNxmfHTgOtGnNoY" download>Download</a>
               </li>
               <li>
                 Design of Robust 635 LBF NOS-Ethanol Rocket Engine for Ground Testing - Zhen Bo Bian (Sep 2021) |
-                <a href="https://www.waterloorocketry.com/pdfs/ZhenBoBianSep2021.pdf" target="_blank" rel="noreferrer">View</a>
+                <a href="https://drive.google.com/file/d/13wcIYlFyis6gbUdSigMnKtdh1-airZBQ/preview" target="_blank" rel="noreferrer">View</a>
                 {' '}
                 |
-                <a href="https://www.waterloorocketry.com/pdfs/ZhenBoBianSep2021.pdf" download>Download</a>
+                <a href="https://drive.google.com/uc?export=download&id=13wcIYlFyis6gbUdSigMnKtdh1-airZBQ" download>Download</a>
               </li>
               <li>
                 Design of Test Stand for High Risk Rocket Engine Testing - Hamza Abuabah (May 2020) |
-                <a href="https://www.waterloorocketry.com/pdfs/HamzaAbuabahMay2020.pdf" target="_blank" rel="noreferrer">View</a>
+                <a href="https://drive.google.com/file/d/13wcIYlFyis6gbUdSigMnKtdh1-airZBQ/preview" target="_blank" rel="noreferrer">View</a>
                 {' '}
                 |
-                <a href="https://www.waterloorocketry.com/pdfs/HamzaAbuabahMay2020.pdf" download>Download</a>
+                <a href="https://drive.google.com/uc?export=download&id=13wcIYlFyis6gbUdSigMnKtdh1-airZBQ" download>Download</a>
               </li>
               <li>
                 Development of a laminate calculator based on Classical Laminate Plate Theory - Shirley Kong (May 2020) |
-                <a href="https://www.waterloorocketry.com/pdfs/ShirleyKongMay2020.pdf" target="_blank" rel="noreferrer">View</a>
+                <a href="https://drive.google.com/file/d/1OKmbffri3W1MqW8Ls_vqQ2R3psECpFIj/preview" target="_blank" rel="noreferrer">View</a>
                 {' '}
                 |
-                <a href="https://www.waterloorocketry.com/pdfs/ShirleyKongMay2020.pdf" download>Download</a>
+                <a href="https://drive.google.com/uc?export=download&id=1OKmbffri3W1MqW8Ls_vqQ2R3psECpFIj" download>Download</a>
               </li>
               <li>
                 Design of a Safety Vent Valve - Matthew Gencher (May 2020) |
-                <a href="https://www.waterloorocketry.com/pdfs/MatthewGencherMay2020.pdf" target="_blank" rel="noreferrer">View</a>
+                <a href="https://drive.google.com/file/d/1di-0h_MMZf5IB7Vwp1JeGVw4BenTNKcX/preview" target="_blank" rel="noreferrer">View</a>
                 {' '}
                 |
-                <a href="https://www.waterloorocketry.com/pdfs/MatthewGencherMay2020.pdf" download>Download</a>
+                <a href="https://drive.google.com/uc?export=download&id=1di-0h_MMZf5IB7Vwp1JeGVw4BenTNKcX" download>Download</a>
               </li>
               <li>
                 Development of a Layout Algorithm for Plumbing System Visualization - Jacob Deery (Sep 2020) |
-                <a href="https://www.waterloorocketry.com/pdfs/JacobDeerySep2020.pdf" target="_blank" rel="noreferrer">View</a>
+                <a href="https://drive.google.com/file/d/15Sd0619EMNpo_Z3IYPA4vw-ykMG10Db6/preview" target="_blank" rel="noreferrer">View</a>
                 {' '}
                 |
-                <a href="https://www.waterloorocketry.com/pdfs/JacobDeerySep2020.pdf" download>Download</a>
+                <a href="https://drive.google.com/uc?export=download&id=15Sd0619EMNpo_Z3IYPA4vw-ykMG10Db6" download>Download</a>
               </li>
             </ul>
           </Content>
